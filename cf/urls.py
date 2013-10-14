1ff27cffffcbbadfdeb31d7c671a4fae2555cc1a
pollitux <raulgranados3014@gmail.com>
2013-10-14 18:41:02 -0500

    Agrgue las url del directorio y archivos estaticos

--- cf/urls.py
+++ cf/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
 	#Urls de la app directorio
     url(r'^',include('directorio.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
--- directorio/views.py
+++ directorio/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def inicio(request):
 	n = negocio.objects.all()
 	template = 'index.html'
